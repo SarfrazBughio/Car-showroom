48d89e3d2d2e695af2327d3441349e453e889b0d
SarfrazBughio <47584891+SarfrazBughio@users.noreply.github.com>
2019-04-16 09:51:12 +0500

    Add files via upload

---
+++ carExMethod.java
@@ -0,0 +1,19 @@
+class carExMethod{
+	private String name;
+	
+	carExMethod(String name){
+		
+		this.name = name;
+	}
+	
+	
+	
+	
+	public  void carName(){
+		
+		System.out.println("Name of this car is "+name);
+	}
+
+
+
+}
